d96bc47834e2e7062924db1b4f47e45a5d454edd
Peter Schär <peter.schaer@be.ch>
2021-05-04 20:55:50 +0200

    CORS-Header in allen Antworten eingebaut.

--- oereb_server/__init__.py
+++ oereb_server/__init__.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
 from pyramid.config import Configurator
+from pyramid.events import NewRequest
+
+def add_cors_headers_response_callback(event):
+    def cors_headers(request, response):
+        response.headers.update({
+        'Access-Control-Allow-Origin': '*',
+        # 'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,OPTIONS',
+        # 'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization',
+        # 'Access-Control-Allow-Credentials': 'true',
+        # 'Access-Control-Max-Age': '1728000',
+        })
+    event.request.add_response_callback(cors_headers)
 
 __version__ = '2.2.2'
 
@@ -13,4 +25,5 @@ def main(global_config, **settings):
     config.add_route('version', '/version')
     config.add_route('home', '/')
     config.scan()
+    config.add_subscriber(add_cors_headers_response_callback, NewRequest)
     return config.make_wsgi_app()
