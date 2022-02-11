4e0b67d23eb4334ec3180d71b656fd4774fb620b
Peter Schär <peter.schaer@be.ch>
2022-02-11 15:10:46 +0100

    #51488: wo_redirect entfernt.

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,7 +1,5 @@
 import requests
-import pytest
 
-#@pytest.mark.xfail(condition="config.getoption('--env')!='dev'", reason="Pyramid-Defaultseite wird in test/prod noch angezeigt (s. #5664)")
 def test_server_root(running_server_instance):
     res = requests.get(running_server_instance)
     assert res.status_code == 404
@@ -17,4 +15,9 @@ def test_config_valid(config):
 def test_debugtoolbar(running_server_instance):
     debugtoolbar_url = running_server_instance + "/_debug_toolbar/"
     res = requests.get(debugtoolbar_url)
+    assert res.status_code == 404
+
+def test_woredirect(running_server_instance, egrid_with_some_plr):
+    url = running_server_instance + "/wo_redirect?egrid=%s&lang=de" % (egrid_with_some_plr)
+    res = requests.get(url)
     assert res.status_code == 404
