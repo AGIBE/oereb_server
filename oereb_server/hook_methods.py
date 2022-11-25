96a7c5439e02e3a815efd15236263af4d65f2539
Peter Schär <peter.schaer@be.ch>
2022-11-25 10:29:07 +0100

    #65757: Hook-Method get_symbol_ref eingefügt.

--- oereb_server/hook_methods.py
+++ oereb_server/hook_methods.py
@@ -48,4 +48,48 @@ def get_surveying_data_update_date(real_estate):
         return datetime.datetime.combine(re.currentness, datetime.time.min)
     finally:
         session.close()
-        
+
+# Diese Hook-Funktion wird nicht genutzt, weil die unter symbol_url
+# referenzierten Bilder teilweise transparent sind (s. #65757)
+# Sie bleibt aber im Code, damit sie gegebenenfalls wieder
+# reaktiviert werden kann.
+def get_symbol_ref(request, record):
+    """
+    Returns the link to the symbol of the specified public law restriction.
+    The link is taken form the field symbol_url.
+    Function is based on pyramid_oereb.contrib.data_sources.standard.hook_methods.get_symbol
+
+    Args:
+        request (pyramid.request.Request): The current request instance.
+        record (pyramid_oereb.core.records.plr.PlrRecord or
+            pyramid_oereb.core.records.view_service.LegendEntryRecord): The record of the public law
+            restriction to get the symbol reference for.
+
+    Returns:
+        uri: The link to the symbol for the specified public law restriction.
+    """
+    plr = None
+    for p in Config.get('plrs'):
+        if str(p.get('code')).lower() == str(record.theme.code).lower():
+            plr = p
+            break
+
+    if plr is None:
+        raise HTTPNotFound('No theme with code {}.'.format(record.theme.code))
+
+    params = plr['source']['params']
+    session = database_adapter.get_session(params.get('db_connection'))
+    try:
+        config_parser = StandardThemeConfigParser(**plr)
+        models = config_parser.get_models()
+        model = models.LegendEntry
+        legend_entry = session.query(model).filter(
+            cast(model.id, Text) == cast(record.identifier, Text)
+        ).first()
+        if legend_entry:
+            symbol_url = getattr(legend_entry, 'symbol_url', None)
+            if symbol_url:
+                return symbol_url
+        raise HTTPNotFound("Symbol nicht gefunden.")
+    finally:
+        session.close()        
