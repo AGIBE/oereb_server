e703612e9943843779b505e558d6548e4f85de05
Peter Schär <peter.schaer@be.ch>
2025-09-04 11:09:58 +0200

    Version erhöht auf 3.4.2

--- src/oereb_server/__init__.py
+++ src/oereb_server/__init__.py
@@ -14,7 +14,7 @@ def add_cors_headers_response_callback(event):
     event.request.add_response_callback(cors_headers)
 
 
-__version__ = "3.4.1"
+__version__ = "3.4.2"
 
 
 def main(global_config, **settings):
